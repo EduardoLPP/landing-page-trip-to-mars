bed9381b6a9c6e20b4c9de5d3af3dce627a4c0b8
eduardopasso <eduardo.pereira@tsea.com.br>
2024-05-18 22:45:08 -0300

    terminando o slide da galeira

---
+++ src/components/Gallery/Index.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Container, ContainerImage, ImageGallery } from './styles'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+export function Gallery() {
+  return (
+    <Container>
+         <Swiper loop style={{padding: "0 47px"}} 
+          navigation={true} className="mySwiper" 
+          modules={[Navigation]} 
+          slidesPerView={3} 
+          spaceBetween={300}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false
+          }}>
+                  <SwiperSlide>
+                    <ContainerImage>
+                      <ImageGallery width={400} height={400}  src='/images/gallery-1.jpg' />
+                    </ContainerImage>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ContainerImage>
+                      <ImageGallery width={400} height={400}  src='/images/gallery-2.jpg' />
+                    </ContainerImage>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ContainerImage>
+                      <ImageGallery width={400} height={400}  src='/images/gallery-3.jpg' />
+                    </ContainerImage>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ContainerImage>
+                      <ImageGallery width={400} height={400}  src='/images/gallery-1.jpg' />
+                    </ContainerImage>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ContainerImage>
+                      <ImageGallery width={400} height={400}  src='/images/gallery-2.jpg' />
+                    </ContainerImage>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ContainerImage>
+                      <ImageGallery width={400} height={400}  src='/images/gallery-3.jpg' />
+                    </ContainerImage>
+                  </SwiperSlide>
+                  
+                </Swiper>
+    </Container>
+  )
+}
---
+++ src/components/Gallery/styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { keyframes } from "styled-components"
+import { pixelToRem } from "../../utils/pixelToRemFunction";
+
+const swiper = keyframes`
+    from {
+        transform: translateX(0)
+    }
+
+    to {
+        transform: translateX(calc(-100px * 7));
+    }
+`;
+
+interface ImageGalleryProps {
+    width?: number;
+    height?: number;
+    src: string; 
+}
+
+interface GalleryFlexProps { 
+    flex?: "row" | "column"; 
+    width?: string;
+    margin?: string;
+    justifyContent?: string;
+    
+}
+
+export const Container = styled.div`
+    display: flex;
+    max-width: ${pixelToRem(1150)};
+    border-radius: ${pixelToRem(12)};
+
+    & .swiper-button-prev {
+        color: var(--mars);
+    }
+    & .swiper-button-next {
+        color: var(--mars);
+    }
+`;
+
+export const ContainerImage = styled.div`
+    display: flex;
+`;
+
+export const ImageGallery = styled.img<ImageGalleryProps>`
+    width: ${(props) => props.width && pixelToRem(props.width)};
+    height: ${(props) => props.height && pixelToRem(props.height)};
+    background-repeat: no-repeat;
+    border-radius: ${pixelToRem(15)};
+    src: ${(props) => props.src};
+    object-fit: cover;
+
+    animation: ${swiper} 20s linear infinite ;
+`;
